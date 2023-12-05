54de4befb932962b3debad02d27b32df164a3458
lyalik <57476711+lyalik@users.noreply.github.com>
2023-12-05 11:09:23 +0300

    Установка и настройка

---
+++ install.php
@@ -0,0 +1,21 @@
+Коллеги, всем добрый день!
+
+По поводу инструкции - должна была подгрузиться автоматически.
+Выкладываю сами ссылки на установку ПО:
+
+➤Установка Git для Windows, MAC, Linux: https://git-scm.com/downloads
+
+➤Установка VSCode для Windows, MAC, Linux: https://code.visualstudio.com/Download
+https://selectel.ru/blog/tutorials/how-to-install-git-to-windows/ - установка Git.
+https://habr.com/ru/articles/490754/ - установка VSC.
+Немного опережаю Ваш семинар по Гиту.
+
+Чтобы использовать коммиты, нужно представить Гиту. Для "авторизации" в Гите потребуется ввести имя и почту в терминале VSC (куда мы и вводим команду "git init").
+Представиться лучше перед тем, как сделали первый коммит.
+
+Для ввода имени используем: git config --global user.name “Your_name”
+Для ввода почты используем: git config --global user.email ваша почта@example.com
+
+Вставляем свое имя и почту, к примеру:
+git config --global user.name “Mikhail”
+git config --global user.email msmerkushov123@yandex.ru
